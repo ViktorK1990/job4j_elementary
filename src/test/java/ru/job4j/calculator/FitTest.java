8bef7b428eb6de1ff9c0b8d6fe2169ad68bf67c8
viktorK1990 <korotchinV1990@yandex.ru>
2023-01-25 12:54:00 +0800

    Тесты для задачи "идеальный вес" [#159458 #193935]

---
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculator;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FitTest {
+
+    @Test
+    void whenMan175then8625() {
+        short in = 175;
+        double expected = 86.25;
+        double out = Fit.manWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    void whenWoman164then6209() {
+        short in = 164;
+        double expected = 62.09;
+        double out = Fit.womanWeight(in);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
