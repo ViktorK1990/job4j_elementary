60eb3836156e18514025a1b7e4efe7a8a5e58cff
viktorK1990 <korotchinV1990@yandex.ru>
2023-01-26 17:56:56 +0800

    Тернарное сравнение [#189 #195724]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -3,8 +3,8 @@ public class Max {
     public static int max(int left, int right) {
         return left > right ? left : right;
     }
-    public static void main(){
-        int rsl = max(10,12);
+    public static void main() {
+        int rsl = max(10, 12);
         System.out.println(rsl);
     }
 }
