8691e51584e6f635052bf8eb33e34bf6feea3a2c
viktorK1990 <korotchinV1990@yandex.ru>
2023-01-30 16:21:57 +0800

    Простое число [#171694 #209824]

---
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,20 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+
+    public static boolean check(int number) {
+        boolean prime = number > 1;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0){
+                prime = false;
+            }
+
+
+        }
+        return prime;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(check(17));
+    }
+}
