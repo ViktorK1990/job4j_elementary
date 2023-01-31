6e47cd211f57b8beef62e8ab643a1953bcd38b93
viktorK1990 <korotchinV1990@yandex.ru>
2023-01-31 12:38:40 +0800

    5.6. Простые числа [#156314 #210113]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -16,6 +16,6 @@ public class CheckPrimeNumber {
     }
 
     public static void main(String[] args) {
-        System.out.println(check(1));
+        System.out.println(check(5));
     }
 }
---
+++ src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+    public static int calc(int finish) {
+        int count = 0;
+        for (int i = 2; i <= finish; i++) {
+            if (CheckPrimeNumber.check(i)) {
+                count += 1;
+
+
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calc(5));
+    }
+}
