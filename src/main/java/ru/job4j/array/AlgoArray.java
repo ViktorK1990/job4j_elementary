7284b9ee7d322a8866ef49f688005b05c669aa2c
viktorK1990 <korotchinV1990@yandex.ru>
2023-02-02 17:26:10 +0800

    Упорядочить массив. [#4441 #213893]

--- src/main/java/ru/job4j/array/AlgoArray.java
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -1,6 +1,6 @@
 package ru.job4j.array;
 
-public class Algoarray {
+public class AlgoArray {
     public static void main(String[] args) {
         int[] array = new int[] {5, 3, 2, 1, 4};
         int temp = array[0];
@@ -19,3 +19,4 @@ public class Algoarray {
         }
     }
 }
+
