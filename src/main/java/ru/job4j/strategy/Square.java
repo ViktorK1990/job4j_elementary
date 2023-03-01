b5765056c7f0f98dbe6081410bf574a9f7e46a4c
viktorK1990 <korotchinV1990@yandex.ru>
2023-03-01 17:34:18 +0800

    Шаблон проектирования - Стратегия

---
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -0,0 +1,15 @@
+package ru.job4j.strategy;
+
+public class Square implements Shape {
+    @Override
+    public String draw() {
+        String ln = System.lineSeparator();
+        return    "----------" +ln
+                + "|" + "\t" + "\t " + "|" +ln
+                + "|" + "\t" + "\t " + "|" +ln
+                + "----------";
+
+
+    }
+}
+
