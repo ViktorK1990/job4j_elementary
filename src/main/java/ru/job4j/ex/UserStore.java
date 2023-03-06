4ff15a6b72fa5c62412ad64f19073f52f298e323
viktorK1990 <korotchinV1990@yandex.ru>
2023-03-06 14:57:11 +0800

    Error - исключения, связанные с работой виртуальной машины

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -25,7 +25,7 @@ public class UserStore {
         if (validate(user)) {
             System.out.println("This user has an access");}
         }
-        catch (UserNotFoundException  | UserInvalidException exception){
+        catch (UserNotFoundException | UserInvalidException exception){
             exception.printStackTrace();
         }
     }
